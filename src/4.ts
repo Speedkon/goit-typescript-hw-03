be813615853e42eb31d93a12b12256c14fe754be
Speedkon <katakononihina@gmail.com>
2023-12-25 18:02:10 +0300

    ts

--- src/4.ts
+++ src/4.ts
@@ -1,5 +1,52 @@
-const key = new Key();
+class Key {
+    private signature: number;
+    constructor() {
+        this.signature = Math.random();
+    }
+    getSignature(): number {
+        return this.signature;
+    }
+}
 
+class Person {
+    private key: Key;
+    constructor(key: Key) {
+        this.key = key;
+    };
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    protected tenants: Person[] = [];
+
+    constructor(key: Key) {
+    this.door = false; 
+    this.key = key;
+    }
+
+    abstract openDoor(key: Key): void;
+
+    comeIn(person: Person):  void {
+        if (this.door) {
+            this.tenants.push(person);
+        };
+    }
+}
+
+class MyHouse extends House {
+    public openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+        }
+    }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
